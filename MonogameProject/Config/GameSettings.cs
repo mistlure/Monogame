4dc11ff3348a4d4ff8dd93d2778e128d0faf5e1f
mistlure <mistlure@outlook.com>
2026-02-05 19:36:47 +0100

    Added CursorEntityId and options for few different menus

--- MonogameProject/Config/GameSettings.cs
+++ MonogameProject/Config/GameSettings.cs
@@ -3,27 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MonogameProject.Enums;
 
 namespace MonogameProject.Config
 {
+    public struct ShopItem
+    {
+        public string Name;
+        public int Price;
+        public TileType? ResultTile;
+        public bool IsCancelButton;
+    }
+
     public static class GameSettings
     {
-        // Map dimensions in tiles
-        // Change these values to adjust the map size
         public const int MapWidth = 30;
         public const int MapHeight = 30;
-
-        // Size of each tile in pixels
-        // Change this value to adjust the tile size
         public static int TileSize = 16;
-
-        // Initial player coins
-        // Change this value to set the starting amount of coins
         public static int PlayerCoins = 120;
 
-        // Cost per tile
-        public const int TileCost = 10;
-        public const int CostGrass = 10;
-        public const int CostSand = 50;
+        public static int CursorEntityId = -1;
+
+        public const int DefaultTileCost = 10;
+
+        public static readonly List<ShopItem> TilePurchaseOptions = new List<ShopItem>
+        {
+            new ShopItem { Name = "X", Price = 0, ResultTile = null, IsCancelButton = true },
+            new ShopItem { Name = "Grass", Price = 10, ResultTile = TileType.Grass, IsCancelButton = false },
+            new ShopItem { Name = "Sand", Price = 15, ResultTile = TileType.Sand, IsCancelButton = false }
+        };
+
+        public static readonly List<ShopItem> GrassDigOptions = new List<ShopItem>
+        {
+            new ShopItem { Name = "X", Price = 0, ResultTile = null, IsCancelButton = true },
+            new ShopItem { Name = "Dig", Price = 0, ResultTile = TileType.Farm, IsCancelButton = false }
+        };
     }
 }
